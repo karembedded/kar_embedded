7b716cfb996d4a53dbb75a4f32766d79b8d4dc44
kalpeshmali <kalpeshmali134@gmail.com>
2018-11-27 16:03:05 +0530

    Updated Assignmeent

--- kal_embedded/Python/Assignment3/Extract_HTML_page.py
+++ kal_embedded/Python/Assignment3/Extract_HTML_page.py
@@ -1,44 +1,32 @@
-import pandas as pd
-url = 'C:\Assignment 3\Buy Huawei Ascend P1 Online at Best Prices In India  Flipkart_com.mht'
-url1='C:\Assignment 3\Big_india.mht'
-for i, df in enumerate(pd.read_html(url)):
-    df.to_csv('xyz.csv',index = False, header = False, mode='a')
-for i, df in enumerate(pd.read_html(url1)):
-    df.to_csv('xyz1.csv',index = False, header = False, mode='a')
-
-with open("out1.txt","w+") as csv1:
-    f=open('xyz.csv','r+')
-    lines=f.readlines()
-    #csv1.writelines(lines[0])
-    csv1.writelines(lines[1])
-    csv1.writelines(lines[6])
-    csv1.writelines(lines[8])
-    csv1.writelines(lines[12])
-    csv1.writelines(lines[16])
-    csv1.writelines(lines[43])
-    csv1.writelines(lines[49])
-    csv1.writelines(lines[57])
-    csv1.writelines(lines[59])
-    csv1.writelines(lines[62])
-    csv1.writelines(lines[64])
-
-with open("out2.txt","w+") as csv1:
-    f=open('xyz1.csv','r+')
-    lines=f.readlines()
-    csv1.writelines(lines[36])
-    csv1.writelines(lines[41])
-    csv1.writelines(lines[43])
-    csv1.writelines(lines[53])
-    csv1.writelines(lines[55])
-    csv1.writelines(lines[80])
-    csv1.writelines(lines[77])
-    csv1.writelines(lines[87])
-    csv1.writelines(lines[88])
-    csv1.writelines(lines[93])
-    csv1.writelines(lines[94])
-f.close()
-csv1.close()
+import sys
 import csv
+with open("out1.txt","w") as csv1:
+    f=open('xyz.csv','r')
+    list1=("Brand","Model Name","SIM Type","Secondary Camera","Primary Camera","Weight","Internal")
+    lines=f.readlines()
+    line1=lines
+    f.seek(0,0)
+    k=0
+    for i in line1:
+        r=range(7)
+        for k in r:
+            if list1[k] in i:
+                csv1.writelines(i)
+    f.close()
+with open("out2.txt","w") as csv1:
+    f=open('xyz1.csv','r')
+    list1=("Brand","Model Name","SIM Type","Secondary Camera","Primary Camera","Weight","Internal")
+
+    lines=f.readlines()
+    line1=lines
+    f.seek(0,0)
+    k=0
+    for i in line1:
+        r=range(7)
+        for k in r:
+            if list1[k] in i:
+                csv1.writelines(i)
+    f.close()
 def read_cell(x, y):
     with open('out2.txt', 'r+') as f:
         reader = csv.reader(f)
@@ -49,14 +37,15 @@ def read_cell(x, y):
                 return cell
             y_count += 1
 f=open("out3.txt",'w+')
-for i in range(0,9):
+f.seek(0, 0)
+for i in range(0,7):
     rd=read_cell(1,i)
     f.write(rd)
     f.write("\n")
 f.close()
 
-f1=open('out1.txt','r+')
-f2=open('out3.txt','r+')
+f1=open('out1.txt','r')
+f2=open('out3.txt','r')
 f3=open('out3.csv','w')
 lines1=f1.readline()
 lines2=f2.readline()
