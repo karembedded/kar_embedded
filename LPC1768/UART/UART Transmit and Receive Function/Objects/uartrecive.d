85b46c05724f8c59eebed002e27fa8b4fb30dead
kalpeshmali <kalpeshmali134@gmail.com>
2019-06-10 17:31:13 +0530

    Commiting All files

--- LPC1768/UART/UART Transmit and Receive Function/Objects/uartrecive.d
+++
@@ -1,7 +0,0 @@
-.\objects\uartrecive.o: uartRecive.c
-.\objects\uartrecive.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\LPC17xx.h
-.\objects\uartrecive.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
-.\objects\uartrecive.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\uartrecive.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
-.\objects\uartrecive.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
-.\objects\uartrecive.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
--- LPC1768/UART/UART Transmit and Receive Function/uartTransmit.c
+++
@@ -1,8 +0,0 @@
-#include "LPC17xx.h"
-void uartTransmit(uint32_t val)
-{
-				if(((LPC_UART0->LSR)&0x20)==0x20)   		/* Check the UnTHR is empty or not.*/
-				{
-					LPC_UART0->THR=val; 
-				}					
-}
